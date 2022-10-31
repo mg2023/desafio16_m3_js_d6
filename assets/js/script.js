180fad8035d8ab732b1d479852b55e0fe4285b3c
mg <amg.vergara@gmail.com>
2022-10-31 09:12:24 -0300

    Se agrega el grafico en funcion del tipo de cambio

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,10 +2,17 @@ const idMontoAConvertir = document.querySelector('#monto-a-convertir')
 const idTipoDeCambio = document.querySelector('#tipo-de-cambio')
 const idBtnConvertir = document.querySelector('#btn-convertir')
 const idResultadoConversion = document.querySelector('#resultado-conversion')
+const idMyChart = document.getElementById('myChart')
+let chart = ''
 
 idBtnConvertir.addEventListener('click', async () => {
   const resultConversion = await conversorMoneda(idMontoAConvertir.value, idTipoDeCambio.value)
   idResultadoConversion.innerHTML = resultConversion.toFixed(2)
+
+  // if (chart) {
+  //   chart.destroy()
+  // }
+  renderGrafica(idTipoDeCambio.value)
 })
 
 // Para convertir necesito monto y el tipo de cambio
@@ -23,4 +30,50 @@ async function solicitarRelacionDeConversion (tipoDeCambio) {
   } catch (error) {
     alert(error.message)
   }
+}
+
+// Creacion de la grafica
+// Precio en el eje y
+// Fecha en el eje x
+async function getAndCreateDataToChart (tipoDeCambio) {
+  // const res = await fetch('https://api.gael.cloud/general/public/sismos')
+  const res = await fetch('https://mindicador.cl/api/' + tipoDeCambio)
+  const dataraw = await res.json()
+
+  // label tiene los valores y las fechas
+  let serie = dataraw.serie.slice(0, 10)
+
+  serie = serie.reverse()
+  const labels = serie.map((e) => {
+    return e.fecha
+  })
+  const data = serie.map((e) => {
+    return e.valor
+  })
+
+  console.log(labels)
+
+  const datasets = [
+    {
+      label: 'Historial Últimos 10 días',
+      borderColor: 'rgb(255, 99, 132)',
+      data
+    }
+  ]
+  return { labels, datasets }
+}
+
+async function renderGrafica (tipoDeCambio) {
+  console.log('debug1')
+  const data = await getAndCreateDataToChart(tipoDeCambio)
+  const config = {
+    type: 'line',
+    data
+  }
+
+  idMyChart.style.backgroundColor = 'white'
+  if (chart) {
+    chart.destroy()
+  }
+  chart = new Chart(idMyChart, config)
 }
